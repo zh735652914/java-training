e18fbd0e73ca6751ae11c6c389507fdfbea6dcb3
zhouhao <zh735652914@gmail.com>
2019-09-07 09:14:27 +0800

    By ZhouHao

--- src/nowcoder/abc.java
+++ src/nowcoder/abc.java
@@ -19,32 +19,25 @@ abcabc
 import java.util.Scanner;
 
 public class abc {
-    char[] A = {'a', 'b', 'c'};
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String S = sc.nextLine();
         abc A = new abc();
-        int ans = A.backtrace(S, 0, 0, 0);
+        long ans = A.Solution(S);
         System.out.println(ans);
     }
 
-
-    public int backtrace(String S, int ans, int pos, int x) {
-        if (S.length() < 3) return 0;
-        for (int i = pos; i < S.length(); i++) {
-            if (S.charAt(i) == A[x]) {
-                x++;
-                if (x < 3) {
-//                    return backtrace(S, ans, ++i, x) + backtrace(S, ans, i + 2, x);
-                    ans = backtrace(S, ans, i + 2, x);
-                } else {
-                    x = 0;
-//                    return backtrace(S, ++ans, ++i, x) + backtrace(S, ++ans, i + 2, x);
-                    ans = backtrace(S, ++ans, i + 2, x);
-                }
+    private long Solution(String S) {
+        long A = 0, AB = 0, ABC = 0;
+        for (int i = 0; i <S.length(); i++) {
+            if(S.charAt(i)=='a'){
+                A++;
+            }else if(S.charAt(i)=='b'){
+                AB=AB+A;
+            }else if (S.charAt(i)=='c'){
+                ABC=ABC+AB;
             }
         }
-        return ans;
+        return ABC;
     }
 }
