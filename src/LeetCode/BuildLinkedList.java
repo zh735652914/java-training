5eea3c8fd47f53750f785bcc13acb81f3cc0791a
zhouhao <zh735652914@gmail.com>
2019-10-31 19:20:16 +0800

    By ZhouHao

---
+++ src/LeetCode/BuildLinkedList.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+public class BuildLinkedList {
+    public ListNode Build(int[] nums) {
+        if (nums.length == 0) return null;
+        ListNode head = new ListNode(nums[0]);
+        ListNode per = head;
+        for (int i = 1; i < nums.length; i++) {
+            ListNode tmp = new ListNode(nums[i]);
+            per.next = tmp;
+            per = tmp;
+        }
+        return head;
+    }
+}
