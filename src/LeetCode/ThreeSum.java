1497b2dc20f8b746b428acacb52575046015bf92
zhouhao <zh735652914@gmail.com>
2019-11-13 13:41:32 +0800

    By ZhouHao

--- src/LeetCode/ThreeSum.java
+++ src/LeetCode/ThreeSum.java
@@ -1,9 +1,8 @@
 package LeetCode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-
 /*
 Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
@@ -21,30 +20,32 @@ A solution set is:
   [-1, -1, 2]
 ]
  */
+
 class ThreeSumSolution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (nums.length < 3) return ans;
         Arrays.sort(nums);
-//        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> ans = new LinkedList<>();
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1] ) continue;
-            int l = i + 1;
-            int r = nums.length - 1;
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if (sum < 0) {
-                    l++;
-                } else if (sum > 0) {
-                    r--;
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+            List<Integer> aAns = new LinkedList<>();
+            aAns.add(nums[i]);
+            int sum = 0 - nums[i], begin = i + 1, end = nums.length - 1;
+            while (begin < end) {
+                if (nums[begin] + nums[end] < sum) {
+                    begin++;
+                } else if (nums[begin] + nums[end] > sum) {
+                    end--;
                 } else {
-                    List<Integer> aAns = new ArrayList<>();
-                    aAns.add(nums[i]);
-                    aAns.add(nums[l]);
-                    aAns.add(nums[r]);
-                    ans.add(aAns);
-                    l++;
-//                    break;
+                    aAns.add(nums[begin]);
+                    aAns.add(nums[end]);
+                    ans.add(new LinkedList<>(aAns));
+                    aAns.remove(aAns.size() - 1);
+                    aAns.remove(aAns.size() - 1);
+                    while (begin < end && nums[begin + 1] == nums[begin]) begin++;
+                    while (begin < end && nums[end - 1] == nums[end]) end--;
+//                    System.out.println("begin=" + begin + "   end=" + end);
+                    begin++;
+                    end--;
                 }
             }
         }
@@ -55,10 +56,9 @@ class ThreeSumSolution {
 public class ThreeSum {
     public static void main(String[] args) {
         ThreeSumSolution TS = new ThreeSumSolution();
-//        int[] nums = {-1,0,1,2,-1,-4};
-        int[] nums = {0, 0, 0};
+//        int[] nums = {-1, 0, 1, 2, -1, -4};
+        int[] nums = {-2, 0, 0, 2, 2};
         List<List<Integer>> ans = TS.threeSum(nums);
         System.out.println("ans=" + ans);
-
     }
 }
