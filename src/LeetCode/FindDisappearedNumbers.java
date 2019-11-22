feab78e675bc4095c84e0b71c4d1865f69ea057f
zhouhao <zh735652914@gmail.com>
2019-11-22 17:14:55 +0800

    By ZhouHao

--- src/LeetCode/FindDisappearedNumbers.java
+++ src/LeetCode/FindDisappearedNumbers.java
@@ -21,26 +21,42 @@ import java.util.Arrays;
 import java.util.List;
 
 class FindDisappearedNumbersSolution {
+    // 自己写的，可以过，但是效率不够
+//    public List<Integer> findDisappearedNumbers(int[] nums) {
+//        List<Integer> ans = new ArrayList<>();
+//        if (nums.length == 0) return ans;
+//        Arrays.sort(nums);
+//        for (int i = 0; i < nums.length; i++) {
+//            if (i == 0 && nums[i] != 1) {
+//                for (int j = 1; j < nums[i]; j++) {
+//                    ans.add(j);
+//                }
+//                continue;
+//            }
+//            if (i == 0) continue;
+//            if (nums[i] - nums[i - 1] > 1) {
+//                for (int j = nums[i - 1] + 1; j < nums[i]; j++) {
+//                    ans.add(j);
+//                }
+//            }
+//        }
+//        for (int i = nums[nums.length - 1] + 1; i <= nums.length; i++) {
+//            ans.add(i);
+//        }
+//        return ans;
+//    }
+
+    //看了discuss后的解答，感觉自己是个智障。。。
     public List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> ans = new ArrayList<>();
-        if (nums.length == 0) return ans;
-        Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
-            if (i == 0 && nums[i] != 1) {
-                for (int j = 1; j < nums[i]; j++) {
-                    ans.add(j);
-                }
-                continue;
-            }
-            if (i == 0) continue;
-            if (nums[i] - nums[i - 1] > 1) {
-                for (int j = nums[i - 1] + 1; j < nums[i]; j++) {
-                    ans.add(j);
-                }
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
             }
         }
-        for (int i = nums[nums.length - 1] + 1; i <= nums.length; i++) {
-            ans.add(i);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) ans.add(i + 1);
         }
         return ans;
     }
@@ -48,8 +64,8 @@ class FindDisappearedNumbersSolution {
 
 public class FindDisappearedNumbers {
     public static void main(String[] args) {
-        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
-//        int[] nums = {5, 4, 6, 7, 9, 3, 10, 9, 5, 6};
+//        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        int[] nums = {5, 4, 6, 7, 9, 3, 10, 9, 5, 6};
         FindDisappearedNumbersSolution findDisappearedNumbersSolution = new FindDisappearedNumbersSolution();
         System.out.println(findDisappearedNumbersSolution.findDisappearedNumbers(nums));
     }
