61320fe6a123c42f6a412b87cdcfde9827df145c
zhouhao <zh735652914@gmail.com>
2020-02-09 12:12:33 +0800

    By ZhouHao

---
+++ src/LeetCode/FlattenNestedListIterator.java
@@ -0,0 +1,51 @@
+package LeetCode;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+interface NestedInteger {
+
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    boolean isInteger();
+
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    Integer getInteger();
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return null if this NestedInteger holds a single integer
+    List<NestedInteger> getList();
+}
+//不会写，但是很重要，第一次接触到写接口
+
+public class FlattenNestedListIterator {
+    static public class NestedIterator implements Iterator<Integer> {
+        Stack<NestedInteger> stack = new Stack<>();
+
+        public NestedIterator(List<NestedInteger> nestedList) {
+            for (int i = nestedList.size() - 1; i >= 0; i--) {
+                stack.push(nestedList.get(i));
+            }
+        }
+
+        @Override
+        public Integer next() {
+            return stack.pop().getInteger();
+        }
+
+        @Override
+        public boolean hasNext() {
+            while (!stack.isEmpty()) {
+                if (stack.peek().isInteger()) {
+                    return true;
+                }
+                NestedInteger cur = stack.pop();
+                for (int i = cur.getList().size() - 1; i >= 0; i--) {
+                    stack.push(cur.getList().get(i));
+                }
+            }
+            return false;
+        }
+    }
+}
