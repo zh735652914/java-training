967e37c896b149a31d7a59bf77b9041f95cdbee9
zhouhao <zh735652914@gmail.com>
2020-03-14 13:58:11 +0800

    By ZhouHao

---
+++ src/LeetCode/ToOffer/maxValue.java
@@ -0,0 +1,57 @@
+package LeetCode.ToOffer;
+/*
+面试题47. 礼物的最大价值
+在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。
+你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。
+给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+
+示例 1:
+
+输入:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+输出: 12
+解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物
+
+
+提示：
+
+0 < grid.length <= 200
+0 < grid[0].length <= 200
+通过次数3,921提交次数5,764
+ */
+
+//智障。。。最简单直白的dp，一开始都没看出来
+public class maxValue {
+    static class Solution {
+        public int maxValue(int[][] grid) {
+            int[][] dp = new int[grid.length][grid[0].length];
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (i == 0 && j == 0) {
+                        dp[0][0] = grid[0][0];
+                    } else if (i == 0) {
+                        dp[i][j] = dp[i][j - 1] + grid[i][j];
+                    } else if (j == 0) {
+                        dp[i][j] = dp[i - 1][j] + grid[i][j];
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                    }
+                }
+            }
+            return dp[grid.length - 1][grid[0].length - 1];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}
+        };
+        System.out.println(new Solution().maxValue(grid));
+    }
+}
