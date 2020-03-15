b778d25a6b9f5b6ec0af7cbb642f2552b5ec9534
zhouhao <zh735652914@gmail.com>
2020-03-15 14:49:06 +0800

    By ZhouHao

--- src/LeetCode/ToOffer/twoSum.java
+++ src/LeetCode/ToOffer/twoSum.java
@@ -1,21 +1,44 @@
 package LeetCode.ToOffer;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
+/*
+面试题57. 和为s的两个数字
+输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
+
+
+
+示例 1：
+
+输入：nums = [2,7,11,15], target = 9
+输出：[2,7] 或者 [7,2]
+示例 2：
+
+输入：nums = [10,26,30,31,47,60], target = 40
+输出：[10,30] 或者 [30,10]
+
+
+限制：
+
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^6
+通过次数5,279提交次数8,054
+ */
+//自己写的超时了。。。双指针的看了讨论才写出来。。。
 public class twoSum {
     static class Solution {
         public int[] twoSum(int[] nums, int target) {
-//            Arrays.sort(nums);
             int[] ans = new int[2];
-            for (int i = 0; i < nums.length - 1; i++) {
-                int found = target - nums[i];
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] == found) {
-                        ans[0] = i;
-                        ans[1] = j;
-                        return ans;
-                    }
+            int low = 0, high = nums.length - 1;
+            while (low < high) {
+                if (nums[low] + nums[high] == target) {
+                    ans[0] = nums[low];
+                    ans[1] = nums[high];
+                    break;
+                } else if (nums[low] + nums[high] > target) {
+                    high--;
+                } else {
+                    low++;
                 }
             }
             return ans;
