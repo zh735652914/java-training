55ec737ce3f38ac352b44beeea472203e83beed5
zhouhao <zh735652914@gmail.com>
2020-04-06 14:22:52 +0800

    By ZhouHao

---
+++ src/LeetCode/ProgrammerInterview/MinStack.java
@@ -0,0 +1,67 @@
+package LeetCode.ProgrammerInterview;
+/*
+面试题 03.02. 栈的最小值
+请设计一个栈，除了常规栈支持的pop与push函数以外，还支持min函数，该函数返回栈元素中的最小值。执行push、pop和min操作的时间复杂度必须为O(1)。
+
+
+示例：
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+通过次数2,674提交次数4,387
+ */
+
+import java.util.Stack;
+
+public class MinStack {
+    /**
+     * initialize your data structure here.
+     */
+    Stack<Integer> stack;
+    Stack<Integer> min;
+
+    public MinStack() {
+        stack = new Stack<>();
+        min = new Stack<>();
+    }
+
+    public void push(int x) {
+        if (min.isEmpty() || x <= min.peek()) {
+            min.push(x);
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        int x = stack.pop();
+        if (x == min.peek()) {
+            min.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());  //-> 返回 -3.
+        minStack.pop();
+        System.out.println(minStack.top());      //--> 返回 0.
+        System.out.println(minStack.getMin());   //--> 返回 -2.
+    }
+
+}
