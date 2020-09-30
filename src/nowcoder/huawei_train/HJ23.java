f79c3e99a55753e34e12d08643c911b62639efc4
zhouhao <zh735652914@gmail.com>
2020-09-30 08:59:37 +0800

    这题很简单

---
+++ src/nowcoder/huawei_train/HJ23.java
@@ -0,0 +1,54 @@
+package nowcoder.huawei_train;
+/*
+ 华为机试
+删除字符串中出现次数最少的字符 相似的企业真题
+时间限制：C/C++ 1秒，其他语言2秒 空间限制：C/C++ 32M，其他语言64M 热度指数：179972
+本题知识点： 字符串
+ 算法知识视频讲解
+校招时部分企业笔试将禁止编程题跳出页面，为提前适应，练习时请使用在线自测，而非本地IDE。
+题目描述
+实现删除字符串中出现次数最少的字符，若多个字符出现次数一样，则都删除。输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序。
+注意每个输入文件有多组输入，即多个字符串用回车隔开
+输入描述:
+字符串只包含小写英文字母, 不考虑非法输入，输入的字符串长度小于等于20个字节。
+
+输出描述:
+删除字符串中出现次数最少的字符后的字符串。
+
+示例1
+输入
+复制
+abcdd
+输出
+复制
+dd
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HJ23 {
+    public void solution(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            min = Math.min(min, map.get(s.charAt(i)));
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) != min) {
+                stringBuilder.append(s.charAt(i));
+            }
+        }
+        System.out.println(stringBuilder.toString());
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            new HJ23().solution(scanner.nextLine());
+        }
+    }
+}
