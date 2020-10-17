c0c65bc8b7dea2121f9a4e13100fd67ab76c450f
zhouhao <zh735652914@gmail.com>
2020-10-17 15:15:32 +0800

    （9. 回文数）很简单

---
+++ src/LeetCode/isPalindrome.java
@@ -0,0 +1,70 @@
+package LeetCode;
+/*
+logo
+探索
+题库
+圈子
+竞赛
+企业
+商店
+
+
+题目描述
+评论 (2.3k)
+题解(2741)
+提交记录
+9. 回文数
+判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+示例 1:
+
+输入: 121
+输出: true
+示例 2:
+
+输入: -121
+输出: false
+解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+示例 3:
+
+输入: 10
+输出: false
+解释: 从右向左读, 为 01 。因此它不是一个回文数。
+进阶:
+
+你能不将整数转为字符串来解决这个问题吗？
+
+通过次数472,785提交次数808,146
+ */
+
+import java.util.Scanner;
+
+public class isPalindrome {
+    static class Solution {
+        public boolean isPalindrome(int x) {
+            if (x < 0) {
+                return false;
+            }
+            if (x < 10) {
+                return true;
+            }
+            String s = String.valueOf(x);
+            int left = 0, right = s.length() - 1;
+            while (left < right) {
+                if (s.charAt(left) != s.charAt(right)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            System.out.println(new Solution().isPalindrome(scanner.nextInt()));
+        }
+    }
+}
