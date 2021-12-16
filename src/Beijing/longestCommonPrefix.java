b33747f1661a80e3a15a5641ee9d1597d611bf8d
zh735652914 <zh735652914@gmail.com>
2021-12-16 23:36:04 +0800

    以前写的更精巧

---
+++ src/Beijing/longestCommonPrefix.java
@@ -0,0 +1,41 @@
+package Beijing;
+
+/*
+https://leetcode-cn.com/problems/longest-common-prefix/
+ */
+public class longestCommonPrefix {
+    static class Solution {
+        public String longestCommonPrefix(String[] strs) {
+            StringBuilder stringBuilder = new StringBuilder();
+            int index = 0;
+            while (true) {
+                if (index >= strs[0].length()) {
+                    break;
+                }
+                boolean flag = false;
+                char ch = strs[0].charAt(index);
+                for (int i = 1; i < strs.length; i++) {
+                    if (index >= strs[i].length()) {
+                        flag = true;
+                        break;
+                    }
+                    if (ch != strs[i].charAt(index)) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag) {
+                    break;
+                }
+                stringBuilder.append(ch);
+                index++;
+            }
+            return stringBuilder.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println(new Solution().longestCommonPrefix(strs));
+    }
+}
