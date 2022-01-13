062647425bdd6dfa6873378ce1b1370fcf22ea20
zh735652914 <zh735652914@gmail.com>
2022-01-13 14:09:46 +0800

    别人的写法思路更清晰，自己智商被碾压

--- src/Beijing/dominantIndex.java
+++ src/Beijing/dominantIndex.java
@@ -5,6 +5,28 @@ https://leetcode-cn.com/problems/largest-number-at-least-twice-of-others/
 
 public class dominantIndex {
     static class Solution {
+        // 找第二大的元素
+        public int dominantIndex2(int[] nums) {
+            if (nums.length == 1) {
+                return 0;
+            }
+            if (nums.length < 1) {
+                return -1;
+            }
+            int max = Integer.MIN_VALUE, second = Integer.MIN_VALUE, index = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] > max) {
+                    second = max;
+                    max = nums[i];
+                    index = i;
+                } else if (nums[i] > second) {
+                    second = nums[i];
+                }
+            }
+            return max >= 2 * second ? index : -1;
+        }
+
+        // 自己写的
         public int dominantIndex(int[] nums) {
             if (nums.length == 1) {
                 return 0;
@@ -34,7 +56,7 @@ public class dominantIndex {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1};
-        System.out.println(new Solution().dominantIndex(nums));
+        int[] nums = {1, 0};
+        System.out.println(new Solution().dominantIndex2(nums));
     }
 }
