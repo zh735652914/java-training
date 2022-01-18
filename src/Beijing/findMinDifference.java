cce92f7018e9d0a775d8858dc4b861bc417cd375
zh735652914 <zh735652914@gmail.com>
2022-01-18 13:41:40 +0800

    自己没什么思路，其实不难

---
+++ src/Beijing/findMinDifference.java
@@ -0,0 +1,32 @@
+package Beijing;
+/*
+https://leetcode-cn.com/problems/minimum-time-difference/
+ */
+
+import java.util.Arrays;
+import java.util.List;
+
+public class findMinDifference {
+    static class Solution {
+        public int findMinDifference(List<String> timePoints) {
+            int[] times = new int[timePoints.size()];
+            for (int i = 0; i < timePoints.size(); i++) {
+                String T = timePoints.get(i);
+                times[i] = Integer.parseInt(T.substring(0, 2)) * 60 + Integer.parseInt(T.substring(3));
+            }
+            Arrays.sort(times);
+            int min = Integer.MAX_VALUE;
+            for (int i = 1; i < times.length; i++) {
+                min = Math.min(min, times[i] - times[i - 1]);
+            }
+            // 这一句很重要
+            return Math.min(min, times[0] + 60 * 24 - times[times.length - 1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"12:12", "00:13"};
+        List<String> timePoints = Arrays.asList(strings);
+        System.out.println(new Solution().findMinDifference(timePoints));
+    }
+}
