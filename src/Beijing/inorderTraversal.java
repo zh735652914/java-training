88f9d9e6db9eb7fab49c4264c8eddf2cf1c11ccf
zh735652914 <zh735652914@gmail.com>
2022-01-19 20:17:30 +0800

    回忆回忆中序遍历

---
+++ src/Beijing/inorderTraversal.java
@@ -0,0 +1,34 @@
+package Beijing;
+/*
+https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class inorderTraversal {
+    static class Solution {
+        List<Integer> ans;
+
+        public List<Integer> inorderTraversal(TreeNode root) {
+            ans = new ArrayList<>();
+            backtrack(root);
+            return ans;
+        }
+
+        private void backtrack(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            backtrack(root.left);
+            ans.add(root.val);
+            backtrack(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] nodes = {"1", "null", "2", "3"};
+        TreeNode root = new BuildTree().LevelBuildTree(nodes);
+        System.out.println(new Solution().inorderTraversal(root));
+    }
+}
