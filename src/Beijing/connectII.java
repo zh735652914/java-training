b47f46ab2ef0ce2ac259be8e8bb35ec00df5a8fa
zh735652914 <zh735652914@gmail.com>
2022-01-25 13:53:21 +0800

    自己不会写

---
+++ src/Beijing/connectII.java
@@ -0,0 +1,69 @@
+package Beijing;
+/*
+https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/
+ */
+
+public class connectII {
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    // 自己没想出来，只想说，牛逼
+    static class Solution {
+        public Node connect(Node root) {
+            if (root == null) {
+                return root;
+            }
+            if (root.left != null && root.right != null) {
+                root.left.next = root.right;
+            }
+            if (root.left != null && root.right == null) {
+                root.left.next = getNext(root.next);
+            }
+            if (root.right != null) {
+                root.right.next = getNext(root.next);
+            }
+            connect(root.right);
+            connect(root.left);
+            return root;
+        }
+
+        private Node getNext(Node root) {
+            if (root == null) {
+                return root;
+            }
+            if (root.left != null) {
+                return root.left;
+            }
+            if (root.right != null) {
+                return root.right;
+            }
+            if (root.next != null) {
+                return getNext(root.next);
+            }
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("这题需要题目给的node结点重新建立完全二叉树！");
+    }
+}
