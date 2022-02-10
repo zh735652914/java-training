9f6478ea5cfb85a9065188715dd7d6e67ccb422e
zh735652914 <zh735652914@gmail.com>
2022-02-10 13:27:39 +0800

    中序遍历就可以了

---
+++ src/Beijing/kthSmallest.java
@@ -0,0 +1,36 @@
+package Beijing;
+/*
+https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+ */
+
+public class kthSmallest {
+    static class Solution {
+        private int count, ans;
+
+        public int kthSmallest(TreeNode root, int k) {
+            count = 0;
+            ans = -1;
+            inorderVisit(root, k);
+            return ans;
+        }
+
+        private void inorderVisit(TreeNode root, int k) {
+            if (root == null) {
+                return;
+            }
+            inorderVisit(root.left, k);
+            count++;
+            if (count == k) {
+                ans = root.val;
+            }
+            inorderVisit(root.right, k);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] nodes = {"5", "3", "6", "2", "4", "null", "null", "1"};
+        TreeNode rooot = new BuildTree().LevelBuildTree(nodes);
+        int k = 3;
+        System.out.println(new Solution().kthSmallest(rooot, k));
+    }
+}
