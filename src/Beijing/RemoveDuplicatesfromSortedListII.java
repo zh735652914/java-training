17264b430cc985d2aedaf14a33c3b39d766500e4
zh735652914 <zh735652914@gmail.com>
2022-02-21 13:34:58 +0800

    在面试比较紧张的情况下，还真不一定写的出来

---
+++ src/Beijing/RemoveDuplicatesfromSortedListII.java
@@ -0,0 +1,44 @@
+package Beijing;
+
+/*
+https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/
+ */
+public class RemoveDuplicatesfromSortedListII {
+    static class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode perAns = new ListNode(-1);
+            perAns.next = head;
+            ListNode last = perAns, p = last.next;
+            while (p != null) {
+                ListNode next = p.next;
+                boolean flag = false;
+                while (next != null && next.val == p.val) {
+                    next = next.next;
+                    flag = true;
+                }
+                if (!flag) {
+                    last = last.next;
+                } else {
+                    last.next = next;
+                }
+                p = last.next;
+            }
+            return perAns.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 3, 3, 4, 4, 5};
+        ListNode perhead = new ListNode(-1);
+        ListNode p = perhead;
+        for (int num : nums) {
+            p.next = new ListNode(num);
+            p = p.next;
+        }
+        ListNode ans = new Solution().deleteDuplicates(perhead.next);
+        while (ans != null) {
+            System.out.print(ans.val + "->");
+            ans = ans.next;
+        }
+    }
+}
