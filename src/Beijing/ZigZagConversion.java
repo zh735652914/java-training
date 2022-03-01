a8d80a51c58ec346145ab4a994bcc183c40791e0
zh735652914 <zh735652914@gmail.com>
2022-03-01 15:30:41 +0800

    自己不会，难受

---
+++ src/Beijing/ZigZagConversion.java
@@ -0,0 +1,38 @@
+package Beijing;
+/*
+https://leetcode-cn.com/problems/zigzag-conversion/
+ */
+
+public class ZigZagConversion {
+    // 自己不会，难受
+    static class Solution {
+        public String convert(String s, int numRows) {
+            if (numRows == 1 || numRows >= s.length()) {
+                return s;
+            }
+            StringBuilder[] stringBuilders = new StringBuilder[numRows];
+            for (int i = 0; i < numRows; i++) {
+                stringBuilders[i] = new StringBuilder();
+            }
+            int index = 0, dir = -1;
+            for (int i = 0; i < s.length(); i++) {
+                stringBuilders[index].append(s.charAt(i));
+                if (index == 0 || index == numRows - 1) {
+                    dir = -dir;
+                }
+                index += dir;
+            }
+            StringBuilder ans = new StringBuilder();
+            for (StringBuilder x : stringBuilders) {
+                ans.append(x);
+            }
+            return ans.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        int numRows = 4;
+        System.out.println(new Solution().convert(s, numRows));
+    }
+}
