97eae342afcfb4ff4c21f569fb78eee7cde2864e
zh735652914 <zh735652914@gmail.com>
2022-03-18 13:19:45 +0800

    从右上角开始搜索就行

---
+++ src/Beijing/Searcha2DMatrix.java
@@ -0,0 +1,34 @@
+package Beijing;
+/*
+https://leetcode-cn.com/problems/search-a-2d-matrix/
+ */
+
+// 从右上角开始搜索就行
+public class Searcha2DMatrix {
+    static class Solution {
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int x = 0, y = matrix[0].length - 1;
+            while (x < matrix.length && y >= 0) {
+                if (target == matrix[x][y]) {
+                    return true;
+                } else if (target < matrix[x][y]) {
+                    y--;
+                } else {
+                    x++;
+                }
+            }
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+//        int[][] matrix = {
+//                {1, 3, 5, 7},
+//                {10, 11, 16, 20},
+//                {23, 30, 34, 60}
+//        };
+        int[][] matrix = {{1, 1}};
+        int target = 3;
+        System.out.println(new Solution().searchMatrix(matrix, target));
+    }
+}
