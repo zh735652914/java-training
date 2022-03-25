36782f47dab14bc2c380877738d96f6cb03e587d
zh735652914 <zh735652914@gmail.com>
2022-03-25 15:16:39 +0800

    官方解答更好

---
+++ src/Beijing/BestTimetoBuyandSellStock.java
@@ -0,0 +1,44 @@
+package Beijing;
+
+public class BestTimetoBuyandSellStock {
+    // 官方解答更好
+    static class Solution {
+        public int maxProfit(int[] prices) {
+            int minPrice = Integer.MAX_VALUE;
+            int ans = 0;
+            for (int i = 0; i < prices.length; i++) {
+                if (minPrice > prices[i]) {
+                    minPrice = prices[i];
+                } else if (prices[i] - minPrice > ans) {
+                    ans = prices[i] - minPrice;
+                }
+            }
+            return ans;
+        }
+    }
+
+    // 自己写的效率不高
+    static class Solution0 {
+        public int maxProfit(int[] prices) {
+            int n = prices.length;
+            int[] min = new int[n];
+            int[] max = new int[n];
+            min[0] = prices[0];
+            max[n - 1] = prices[n - 1];
+            for (int i = 1; i < n; i++) {
+                min[i] = Math.min(min[i - 1], prices[i]);
+                max[n - i - 1] = Math.max(max[n - i], prices[n - i - 1]);
+            }
+            int ans = max[0] - min[0];
+            for (int i = 1; i < n; i++) {
+                ans = Math.max(ans, max[i] - min[i]);
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(new Solution().maxProfit(prices));
+    }
+}
