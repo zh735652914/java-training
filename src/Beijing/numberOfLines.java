fdfc8e13092bf5c493a249e0539e75032c4fbd1d
zh735652914 <zh735652914@gmail.com>
2022-04-12 13:17:13 +0800

    简单题

---
+++ src/Beijing/numberOfLines.java
@@ -0,0 +1,33 @@
+package Beijing;
+/*
+https://leetcode-cn.com/problems/number-of-lines-to-write-string/
+ */
+
+public class numberOfLines {
+    static class Solution {
+        public int[] numberOfLines(int[] widths, String s) {
+            if (s.length() < 1) {
+                return new int[]{0, 0};
+            }
+            int row = 1;
+            int col = 0;
+            for (int i = 0; i < s.length(); i++) {
+                int x = widths[s.charAt(i) - 'a'];
+                col += x;
+                if (col > 100) {
+                    row++;
+                    col = x;
+                }
+            }
+            return new int[]{row, col};
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] widths = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+                10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+        String S = "abcdefghijklmnopqrstuvwxyz";
+        int[] ans = new Solution().numberOfLines(widths, S);
+        System.out.println(ans[0] + ", " + ans[1]);
+    }
+}
