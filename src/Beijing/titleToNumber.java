3909a5c88453b73153a49c7b50395d582b28df6c
zh735652914 <zh735652914@gmail.com>
2022-04-13 20:51:21 +0800

    简单题

---
+++ src/Beijing/titleToNumber.java
@@ -0,0 +1,23 @@
+package Beijing;
+/*
+https://leetcode-cn.com/problems/excel-sheet-column-number/
+ */
+
+// 简单题
+public class titleToNumber {
+    static class Solution {
+        public int titleToNumber(String columnTitle) {
+            int ans = 0;
+            for (int i = 0; i < columnTitle.length(); i++) {
+                ans *= 26;
+                ans += columnTitle.charAt(i) - 'A' + 1;
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        String columnTitle = "ZY";
+        System.out.println(new Solution().titleToNumber(columnTitle));
+    }
+}
