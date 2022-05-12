d28f1b2ba888970f430f1cd3c0b8005d612528fd
zh735652914 <zh735652914@gmail.com>
2022-05-12 21:35:40 +0800

    这题比较简单

---
+++ src/Beijing/hammingWeight.java
@@ -0,0 +1,26 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/number-of-1-bits/
+ */
+
+// 这题比较简单
+public class hammingWeight {
+    static class Solution {
+        // you need to treat n as an unsigned value
+        public int hammingWeight(int n) {
+            int count = 0;
+            for (int i = 0; i < 32 && n != 0; i++) {
+                if ((n & 1) == 1) {
+                    count++;
+                }
+                n >>>= 1;
+            }
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = -3;
+        System.out.println(new Solution().hammingWeight(n));
+    }
+}
