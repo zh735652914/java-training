0096ef207b422e4b1b11682f2b10541160d20f96
zh735652914 <zh735652914@gmail.com>
2022-05-19 19:04:52 +0800

    这？？？？？

---
+++ src/Beijing/minimumMovestoEqualArrayElementsII.java
@@ -0,0 +1,27 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/minimum-moves-to-equal-array-elements-ii/
+ */
+
+import java.util.Arrays;
+
+// 这？？？？？
+public class minimumMovestoEqualArrayElementsII {
+    static class Solution {
+        public int minMoves2(int[] nums) {
+            int mid = nums.length / 2;
+            Arrays.sort(nums);
+            int ans = 0;
+            for (int i = 0; i < mid; i++) {
+                ans += nums[mid] - nums[i];
+                ans += nums[nums.length - 1 - i] - nums[mid];
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 10, 2, 9};
+        System.out.println(new Solution().minMoves2(nums));
+    }
+}
