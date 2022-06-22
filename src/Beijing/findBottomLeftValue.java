921cf4d28e29537eea442aa67d0a28965f9cdee0
zh735652914 <zh735652914@gmail.com>
2022-06-22 22:21:53 +0800

    自己写的dfs

---
+++ src/Beijing/findBottomLeftValue.java
@@ -0,0 +1,37 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/find-bottom-left-tree-value/
+ */
+
+// 自己写的dfs
+public class findBottomLeftValue {
+    static class Solution {
+        private int ans;
+        private int ansLevel;
+
+        public int findBottomLeftValue(TreeNode root) {
+            ans = root.val;
+            ansLevel = 1;
+            dfs(root, 1);
+            return ans;
+        }
+
+        private void dfs(TreeNode root, int level) {
+            if (root == null) {
+                return;
+            }
+            dfs(root.left, level + 1);
+            if (level > ansLevel) {
+                ansLevel = level;
+                ans = root.val;
+            }
+            dfs(root.right, level + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] nods = {"1", "2", "3", "4", "null", "5", "6", "null", "null", "7"};
+        TreeNode root = new BuildTree().LevelBuildTree(nods);
+        System.out.println(new Solution().findBottomLeftValue(root));
+    }
+}
