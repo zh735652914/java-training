ef709544927f6999d815dddf6dcd00bc1b5d51b4
zh735652914 <zh735652914@gmail.com>
2022-07-10 15:32:55 +0800

    怎么说呢，还是要看看

---
+++ src/Beijing/reverseInteger_7.java
@@ -0,0 +1,27 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/reverse-integer/
+ */
+
+public class reverseInteger_7 {
+    // 怎么说呢，还是要看看
+    static class Solution {
+        public int reverse(int x) {
+            int n = 0;
+            while (x != 0) {
+                if (n < Integer.MIN_VALUE / 10 || n > Integer.MAX_VALUE / 10) {
+                    return 0;
+                }
+                n *= 10;
+                n += x % 10;
+                x /= 10;
+            }
+            return n;
+        }
+    }
+
+    public static void main(String[] args) {
+        int x = -123;
+        System.out.println(new Solution().reverse(x));
+    }
+}
