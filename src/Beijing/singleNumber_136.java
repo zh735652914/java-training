074a2940af637e21e046e4951bc2f84e180fb8ff
zh735652914 <zh735652914@gmail.com>
2022-07-19 18:11:37 +0800

    第三次写了

---
+++ src/Beijing/singleNumber_136.java
@@ -0,0 +1,22 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/single-number/
+ */
+
+public class singleNumber_136 {
+    // 第三次写了
+    static class Solution {
+        public int singleNumber(int[] nums) {
+            int ans = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                ans ^= nums[i];
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println(new Solution().singleNumber(nums));
+    }
+}
