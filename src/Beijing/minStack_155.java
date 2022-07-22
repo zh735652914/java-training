7d8ae090eb1f25a477030271e4a516dcfc1297a0
zh735652914 <zh735652914@gmail.com>
2022-07-22 13:19:05 +0800

    就是注意特殊情况就行

---
+++ src/Beijing/minStack_155.java
@@ -0,0 +1,52 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/min-stack/
+ */
+
+import java.util.Stack;
+
+// 就是注意特殊情况就行
+public class minStack_155 {
+    static class MinStack {
+        private final Stack<Integer> stack;
+        private final Stack<Integer> minStack;
+
+        public MinStack() {
+            stack = new Stack<>();
+            minStack = new Stack<>();
+        }
+
+        public void push(int val) {
+            stack.push(val);
+            if (minStack.isEmpty() || val <= minStack.peek()) {
+                minStack.push(val);
+            }
+        }
+
+        public void pop() {
+            int val = stack.pop();
+            if (val == minStack.peek()) {
+                minStack.pop();
+            }
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(0);
+        minStack.push(1);
+        minStack.push(0);
+        System.out.println(minStack.getMin()); // return -3
+        minStack.pop();
+        System.out.println(minStack.top());   // return 0
+        System.out.println(minStack.getMin()); // return -2
+    }
+}
