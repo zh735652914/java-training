14009c65737db9d7b7cb0ce3a6a2461f605f1295
zh735652914 <zh735652914@gmail.com>
2022-08-02 19:05:42 +0800

    重点是解析的推理，这个还是要看看

---
+++ src/Beijing/nimGame_292.java
@@ -0,0 +1,18 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/nim-game/
+ */
+
+// 重点是解析的推理，这个还是要看看
+public class nimGame_292 {
+    static class Solution {
+        public boolean canWinNim(int n) {
+            return n % 4 != 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(new Solution().canWinNim(n));
+    }
+}
