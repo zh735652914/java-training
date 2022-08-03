5202918be91505c79cd453f9608f693e5bbb780b
zh735652914 <zh735652914@gmail.com>
2022-08-03 13:07:25 +0800

    很没意思的题目

---
+++ src/Beijing/reverseString_344.java
@@ -0,0 +1,29 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/reverse-string/
+ */
+
+// 很没意思的题目
+public class reverseString_344 {
+    static class Solution {
+        public void reverseString(char[] s) {
+            int left = 0, right = s.length - 1;
+            char tmp;
+            while (left < right) {
+                tmp = s[left];
+                s[left] = s[right];
+                s[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        new Solution().reverseString(s);
+        for (char x : s) {
+            System.out.print(x + ", ");
+        }
+    }
+}
