8bfd5ef9e0ead6b29e420cc184d0ae0de6a818b8
zh735652914 <zh735652914@gmail.com>
2022-09-08 13:21:37 +0800

    自己没想到怎么写

---
+++ src/Beijing/constructArray.java
@@ -0,0 +1,29 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/beautiful-arrangement-ii/
+ */
+
+public class constructArray {
+    // 自己没想到怎么写
+    static class Solution {
+        public int[] constructArray(int n, int k) {
+            int[] ans = new int[n];
+            int index = 0;
+            for (int i = 1; i < (n - k); i++) {
+                ans[index++] = i;
+            }
+            for (int i = n - k, j = n; i <= j; i++, j--) {
+                ans[index++] = j;
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3, k = 2;
+        int[] ans = new Solution().constructArray(n, k);
+        for (int x : ans) {
+            System.out.print(x + ", ");
+        }
+    }
+}
