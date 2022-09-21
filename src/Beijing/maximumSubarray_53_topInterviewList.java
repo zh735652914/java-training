9668f074090f98711bf452555123ba1b0745c28d
zh735652914 <zh735652914@gmail.com>
2022-09-21 17:25:43 +0800

    第四次了，自己还是不会写

---
+++ src/Beijing/maximumSubarray_53_topInterviewList.java
@@ -0,0 +1,24 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/maximum-subarray/
+ */
+
+public class maximumSubarray_53_topInterviewList {
+    // 第四次了，自己还是不会写
+    static class Solution {
+        public int maxSubArray(int[] nums) {
+            int ans = nums[0];
+            int sum = 0;
+            for (int num : nums) {
+                sum = Math.max(sum + num, num);
+                ans = Math.max(ans, sum);
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(new Solution().maxSubArray(nums));
+    }
+}
