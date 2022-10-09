2b396b34df532f77e186fc8659565a200347c851
zh735652914 <zh735652914@gmail.com>
2022-10-09 17:50:34 +0800

    注意数字

---
+++ src/Beijing/validPalindrome_125_topInterviewList.java
@@ -0,0 +1,34 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/valid-palindrome/
+ */
+
+public class validPalindrome_125_topInterviewList {
+    // 注意数字
+    static class Solution {
+        public boolean isPalindrome(String s) {
+            s = s.toLowerCase();
+            int len = s.length();
+            int left = 0, right = len - 1;
+            while (left < right) {
+                while (left < right && !Character.isLetter(s.charAt(left)) && !Character.isDigit(s.charAt(left))) {
+                    left++;
+                }
+                while (left < right && !Character.isLetter(s.charAt(right)) && !Character.isDigit(s.charAt(right))) {
+                    right--;
+                }
+                if (s.charAt(left) != s.charAt(right)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "0P";
+        System.out.println(new Solution().isPalindrome(s));
+    }
+}
