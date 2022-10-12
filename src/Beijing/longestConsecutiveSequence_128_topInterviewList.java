95a50b36e48cfaab3dfce5be9482ceaba6adc60c
zh735652914 <zh735652914@gmail.com>
2022-10-12 10:50:53 +0800

    第三次写了，自己还是不会

---
+++ src/Beijing/longestConsecutiveSequence_128_topInterviewList.java
@@ -0,0 +1,36 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/longest-consecutive-sequence/
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class longestConsecutiveSequence_128_topInterviewList {
+    // 第三次写了，自己还是不会
+    static class Solution {
+        public int longestConsecutive(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num);
+            }
+            int ans = 0;
+            for (int num : set) {
+                if (!set.contains(num - 1)) {
+                    int count = 1;
+                    while (set.contains(num + 1)) {
+                        count++;
+                        num = num + 1;
+                    }
+                    ans = Math.max(ans, count);
+                }
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println(new Solution().longestConsecutive(nums));
+    }
+}
