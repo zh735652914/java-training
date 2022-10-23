1df4d0a94bbc5509c3f2cf21a9d898b08fa3941c
zh735652914 <zh735652914@gmail.com>
2022-10-23 19:38:18 +0800

    注意特殊情况

---
+++ src/Beijing/minStack_155_topInterviewList.java
@@ -0,0 +1,51 @@
+package Beijing;
+
+import java.util.Objects;
+import java.util.Stack;
+
+
+// 注意特殊情况
+public class minStack_155_topInterviewList {
+    static class MinStack {
+        private final Stack<Integer> stack;
+        private final Stack<Integer> min;
+
+        public MinStack() {
+            stack = new Stack<>();
+            min = new Stack<>();
+        }
+
+        public void push(int val) {
+            stack.push(val);
+            if (min.isEmpty() || val <= min.peek()) {
+                min.push(val);
+            }
+        }
+
+        public void pop() {
+            if (Objects.equals(stack.peek(), min.peek())) {
+                min.pop();
+            }
+            stack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return min.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin()); // return -3
+        minStack.pop();
+        System.out.println(minStack.top());    // return 0
+        System.out.println(minStack.getMin()); // return -2
+    }
+}
