8c38728dced62c674459b337d0dc48c767a7dc2c
zh735652914 <zh735652914@gmail.com>
2022-11-04 14:03:18 +0800

    自己没有思路

---
+++ src/Beijing/ReachNumber.java
@@ -0,0 +1,23 @@
+package Beijing;
+/*
+https://leetcode.cn/problems/reach-a-number/
+ */
+
+public class ReachNumber {
+    // 自己没有思路
+    static class Solution {
+        public int reachNumber(int target) {
+            target = Math.abs(target);
+            int s = 0, n = 0;
+            while (s < target || ((s - target) & 1) == 1) {
+                s += ++n;
+            }
+            return n;
+        }
+    }
+
+    public static void main(String[] args) {
+        int target = 2;
+        System.out.println(new Solution().reachNumber(target));
+    }
+}
