e4b4b1eb3c056e77a80466a1f7b3644dff162f0a
zh735652914 <zh735652914@gmail.com>
2023-01-29 16:14:37 +0800

    【设计模式】学习适配器模式

---
+++ src/Beijing/DesignPattern/Adapter/Interface/V5Power.java
@@ -0,0 +1,9 @@
+package Beijing.DesignPattern.Adapter.Interface;
+
+/**
+ * @author zhouhao
+ * @date 2023/1/29 16:08
+ */
+public interface V5Power {
+    int provideV5Power();
+}
---
+++ src/Beijing/DesignPattern/Adapter/V220Power.java
@@ -0,0 +1,12 @@
+package Beijing.DesignPattern.Adapter;
+
+/**
+ * @author zhouhao
+ * @date 2023/1/29 16:10
+ */
+public class V220Power {
+    public int provideV220Power() {
+        System.out.println("我提供220V交流电压");
+        return 220;
+    }
+}
