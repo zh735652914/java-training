37a5a2ba4fb972b598aed86ff2f012e28fa88ac7
zh735652914 <zh735652914@gmail.com>
2023-06-08 13:01:32 +0800

    属于简单题

---
+++ src/beijing/GroupAnagrams_49_HotOneHundred.java
@@ -0,0 +1,46 @@
+package beijing;
+/*
+https://leetcode.cn/problems/group-anagrams/
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author zhouhao
+ * @date 2023/6/8 12:46
+ */
+// 属于简单题
+public class GroupAnagrams_49_HotOneHundred {
+    static class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> map = new HashMap<>();
+            for (String str : strs) {
+                String key = getKey(str);
+                if (map.containsKey(key)) {
+                    map.get(key).add(str);
+                } else {
+                    List<String> list = new ArrayList<>();
+                    list.add(str);
+                    map.put(key, list);
+                }
+            }
+            return new ArrayList<>(map.values());
+        }
+
+        private String getKey(String str) {
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            return Arrays.toString(ch);
+        }
+    }
+
+    public static void main(String[] args) {
+//        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        String[] strs = {""};
+        System.out.println(new Solution().groupAnagrams(strs));
+    }
+}
