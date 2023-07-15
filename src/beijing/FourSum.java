d4d676ab18e80855051e6dc0fe07908b904ce0b6
zh735652914 <zh735652914@gmail.com>
2023-07-15 11:32:39 +0800

    不能错啊

---
+++ src/beijing/FourSum.java
@@ -0,0 +1,67 @@
+package beijing;
+/*
+https://leetcode.cn/problems/4sum/
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author zhouhao
+ * @date 2023/7/15 10:59
+ */
+public class FourSum {
+    // 不能错啊
+    static class Solution {
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            List<List<Integer>> ans = new ArrayList<>();
+            if (nums.length < 4) {
+                return ans;
+            }
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length - 3; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                    break;
+                }
+                for (int j = i + 1; j < nums.length - 2; j++) {
+                    if (j > i + 1 && nums[j] == nums[j - 1]) {
+                        continue;
+                    }
+                    if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                        break;
+                    }
+                    int left = j + 1, right = nums.length - 1;
+                    while (left < right) {
+                        long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                        if (sum == target) {
+                            ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                            while (left < right && nums[left] == nums[left + 1]) {
+                                left++;
+                            }
+                            left++;
+                            while (left < right && nums[right] == nums[right - 1]) {
+                                right--;
+                            }
+                            right--;
+                        } else if (sum < target) {
+                            left++;
+                        } else {
+                            right--;
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        System.out.println(new Solution().fourSum(nums, target));
+    }
+}
