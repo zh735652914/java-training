b4a1081359701aef8315adde4aab2f84be86fcc0
zhouhao <zh735652914@gmail.com>
2023-07-17 20:57:54 +0800

    测试

--- src/beijing/test.java
+++ src/beijing/test.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("输入题目名");
+        System.out.println("输入题目名:");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         String[] words = name.split(" ");
