28901b94cda2141cfd5bb737244601809cbfdc4b
zhouhao <zh735652914@gmail.com>
2023-08-19 11:30:35 +0800

    毫无意义的题目

---
+++ src/beijing/AddTwoIntegers_2235.java
@@ -0,0 +1,22 @@
+package beijing;
+/*
+https://leetcode.cn/problems/add-two-integers/description/
+ */
+
+/**
+ * @author zhouhao
+ * @date 2023/8/19 11:28
+ */
+public class AddTwoIntegers_2235 {
+    // 这题？？？？？侮辱人呢
+    static class Solution {
+        public int sum(int num1, int num2) {
+            return num1 + num2;
+        }
+    }
+
+    public static void main(String[] args) {
+        int num1 = 12, num2 = 5;
+        System.out.println(new Solution().sum(num1, num2));
+    }
+}
