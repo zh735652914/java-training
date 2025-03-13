67aeab9fd695d8b98407249cf0b85a7f36dcd25b
zhouhao <zh735652914@gmail.com>
2025-03-13 08:24:57 +0800

    hot100-终于自己写出来一题

---
+++ src/main/java/next/LeetCode33.java
@@ -0,0 +1,43 @@
+package next;
+
+import org.junit.Test;
+
+/**
+ * @author zhouhao
+ * @date 2025/3/13 08:17
+ * https://leetcode.cn/problems/search-in-rotated-sorted-array/
+ */
+public class LeetCode33 {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] >= nums[left]) {
+                // 左边有序
+                if (nums[mid] > target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // 右边有序
+                if (nums[mid] < target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Test
+    public void test() {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        System.out.println(search(nums, target));
+    }
+}
