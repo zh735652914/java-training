31ae520e9afe234c1a7fe4b5348a5e8a454d2e84
zhouhao <zh735652914@gmail.com>
2025-03-18 08:51:22 +0800

    hot100-64自己写出来，很朴素的dp

---
+++ src/main/java/next/LeetCode64.java
@@ -0,0 +1,33 @@
+package next;
+
+import org.junit.Test;
+
+/**
+ * @author zhouhao
+ * @date 2025/3/18 08:44
+ * https://leetcode.cn/problems/minimum-path-sum/
+ */
+public class LeetCode64 {
+    public int minPathSum(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < grid.length; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < grid[0].length; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[i].length; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[grid.length - 1][grid[0].length - 1];
+    }
+
+    @Test
+    public void test() {
+        int[][] grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        System.out.println(minPathSum(grid));
+    }
+}
