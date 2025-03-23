44f2b1a9d587f99f880ebe420491cd9a2e0da3c2
zhouhao <zh735652914@gmail.com>
2025-03-23 11:54:54 +0800

    hot100-102这个写了很多遍

---
+++ src/main/java/next/LeetCode102.java
@@ -0,0 +1,50 @@
+package next;
+
+import next.utils.BuildTree;
+import next.utils.TreeNode;
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * @author zhouhao
+ * @date 2025/3/23 11:45
+ */
+public class LeetCode102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> aLevel = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                aLevel.add(current.val);
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+            ans.add(aLevel);
+        }
+        return ans;
+    }
+
+    @Test
+    public void test() {
+        String nodes = "[3,9,20,null,null,15,7]";
+        BuildTree buildTree = new BuildTree();
+        assertEquals("[[3], [9, 20], [15, 7]]", String.valueOf(levelOrder(buildTree.LevelBuildTree(nodes))));
+    }
+}
